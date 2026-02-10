3b64c302ddd09cc60ac8f179966fdbb19e140801
Rhyan121121 <rhyanaa1211@gmail.com>
2026-02-09 23:31:46 -0300

    feat:Adding the main CLI program

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,18 @@
 void main() {
+    Scanner sc = new Scanner(System.in);
+    final String WELCOME_MESSAGE = "Bem vindo a criação de conta !";
+
+    System.out.println(WELCOME_MESSAGE);
+
+    System.out.println("Digite seu nome:");
+    var name = sc.nextLine();
+    System.out.println("Digite sua Agência:");
+    var agency = sc.nextLine();
+    System.out.println("Digite o número da sua Conta:");
+    int accountNumber = sc.nextInt();
+    System.out.println("Digite sua Saldo:");
+    double saldo = sc.nextDouble();
+
+    System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo R$ %.2f já está disponível para saque !",name,agency,accountNumber,saldo);
 
     }
